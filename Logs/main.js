7003fe51173d8850a5f36bd977ed99576b3fc313
DerNeuanfang <JanZumdick@web.de>
2017-07-05 16:38:40 +0200

    DerNeuanfang
    Hochgeladen Version 1.0

---
+++ Logs/main.js
@@ -0,0 +1,251 @@
+var App = (new function () {
+	var	instance			= this;
+	var logaktiv = false;
+	var Bot = undefined;
+	var log = {};
+	
+	this.onUserJoined = function onUserJoined(user) {
+		if(!logaktiv){
+			return;
+		}
+		
+		log['txt'].push('°GG°'+user.getNick().escapeKCode() + ' betritt den Channel.°r°');
+	}
+	
+	this.onAppStart = function onAppStart() {
+		Bot = KnuddelsServer.getDefaultBotUser();
+		logaktiv = false;
+	}
+	
+	this.onPublicMessage = function(publicMessage){
+		var user = publicMessage.getAuthor();
+		var txt = publicMessage.getText();
+		
+		log['txt'].push(user.getNick() + ': '+txt);
+	},
+	
+	this.chatCommands = {
+		
+		
+		addc:function(user, params){
+			if(!user.isAppManager()){
+				return;
+			}
+			
+			if(!logaktiv){
+				user.sendPrivateMessage('Es ist kein Log aktiv');
+				return;
+			}
+			
+			log['txt'].push('°GG°Log-Kommentar' + ': '+params+'°r°');
+			
+			user.sendPrivateMessage('Der Kommentar:°#° '+params+'°#° Wurde Hinzugefügt und ist im Log sichtbar.');
+			
+			
+		},
+		startlog:function(user){
+			if(!user.isAppManager()){
+				return;
+			}
+			
+			logaktiv = true;
+			var times = new Date().getTime();
+			
+			var id = KnuddelsServer.getPersistence().addNumber('LogIds', 1);
+			var users			= KnuddelsServer.getChannel().getOnlineUsers(UserType.Human);
+			
+			log = {id: id, datum: times, uir: users, txt:[]};
+			
+			
+			for(i=0;i<users.length;i++){
+				var ids = users[i].getPersistence().getObject('UserLogIds', []);
+				ids.push(id);
+				users[i].getPersistence().setObject('UserLogIds', ids);
+			}
+			
+			user.sendPrivateMessage('Alle öffentliche Logs werden nun gespeichert. Die ID dafür ist: '+id);
+			
+		},
+		
+		resetalllog:function(user){
+			if(!user.isAppManager()){
+				return;
+			}
+			
+			if(logaktiv){
+				user.sendPrivateMessage('Es ist noch ein Log aktiv.');
+				return;
+			}
+			KnuddelsServer.getPersistence().setObject('_logsdata', []);
+			
+			
+			UserPersistenceObjects.deleteAll('UserLogIds');
+		},
+		
+		stoplog:function(user){
+			if(!user.isAppManager()){
+				return;
+			}
+			
+			if(!logaktiv){
+				user.sendPrivateMessage('Es ist kein Log aktiv.');
+				return;
+			}
+			
+			logaktiv = false;
+			
+			var id = KnuddelsServer.getPersistence().getNumber('LogIds', 0);
+			
+			KnuddelsServer.getPersistence().setObject('_logs'+id, log);
+			
+			var logids = KnuddelsServer.getPersistence().getObject('_logsdata', []);
+			logids.push(id);
+			KnuddelsServer.getPersistence().setObject('_logsdata', logids);
+			
+			user.sendPrivateMessage('Der Log wurde gestoppt und gespeichert.');
+			
+			
+		},
+		
+		dellog:function(user, params){
+			if(!user.isAppManager()){
+				return;
+			}
+			
+			if(logaktiv){
+				user.sendPrivateMessage('Die Aktiven Logs, müssen erst deaktiviert werden.');
+				return;
+			}
+			
+			var logids = KnuddelsServer.getPersistence().getObject('_logsdata', []);
+			params = parseInt(params);
+			
+			if(logids.indexOf(params) >= 0){
+				logids.splice(logids.indexOf(params),1);
+				KnuddelsServer.getPersistence().setObject('_logsdata', logids);
+				var logdata = KnuddelsServer.getPersistence().getObject('_logs'+params, {});
+				
+				var users = logdata.uir;
+				
+				for(i=0;i<users.length;i++){
+					var ids = users[i].getPersistence().getObject('UserLogIds', []);
+					ids.splice(ids.indexOf(params),1);
+					users[i].getPersistence().setObject('UserLogIds', ids);
+				}
+				
+				KnuddelsServer.getPersistence().deleteObject('_logs'+params);
+				
+				user.sendPrivateMessage('Log '+params+' wurde gelöscht.');
+				
+			} else {
+				user.sendPrivateMessage('Ich kenne diesen Log leider nicht.');
+			}
+		},
+		
+		showlog:function(user, params){
+			if(!user.isAppManager()){
+				return;
+			}
+			
+			if(logaktiv){
+				user.sendPrivateMessage('Die Aktiven Logs, müssen erst deaktiviert werden.');
+				return;
+			}
+			var logids = KnuddelsServer.getPersistence().getObject('_logsdata', []);
+			
+			if(params.length == 0){
+				var msg = 'Übersicht der Logs:°#°'
+				for(i=0;i<logids.length;i++){
+					var logdata = KnuddelsServer.getPersistence().getObject('_logs'+logids[i], {});
+					
+					msg += ' °>ID:'+logids[i]+' - Am: '+ new Date(logdata.datum)+' mit ' + logdata.uir.join(',')+'|/showlog '+logids[i]+'<° °YY° °>Löschen|/dellog '+logids[i]+'<°°r° °#°'
+				}
+				
+				user.sendPrivateMessage(msg);
+			} else {
+				params = parseInt(params);
+				if(logids.indexOf(params) == -1){
+					user.sendPrivateMessage('Ich finde keinen Log mit der ID '+params.escapeKCode());
+					return;
+				} else {
+					//logdata = {id: id, datum: times, uir: users, txt:[]};
+					var logdata = KnuddelsServer.getPersistence().getObject('_logs'+params, {});
+					
+					var msg = 'Log '+logdata.id + ' vom '+ new Date(logdata.datum) +' mit '+logdata.uir.join(',')+' °#°'
+						msg += logdata.txt.join('°#°');
+						user.sendPrivateMessage(msg);
+				}		
+			}
+	
+		},
+		
+		searchlog:function(user,params){
+			if(!user.isAppManager()){
+				return;
+			}
+			
+			if(logaktiv){
+				user.sendPrivateMessage('Die Aktiven Logs, müssen erst deaktiviert werden.');
+				return;
+			}
+			
+			var UserAccess = KnuddelsServer.getUserAccess();
+			
+			var u = UserAccess.exists(params)
+			if(!u){
+				user.sendPrivateMessage('Ich kenne den User nicht.');
+				return;
+			}
+					
+			var uId = UserAccess.getUserId(params);
+				
+			if(UserAccess.mayAccess(uId)) {
+				
+				var knuser = UserAccess.getUserById(uId);
+				
+				var logids = knuser.getPersistence().getObject('UserLogIds', []);
+				var msg = 'Übersicht der Logs °#°'
+				for(i=0;i<logids.length;i++){
+					var logdata = KnuddelsServer.getPersistence().getObject('_logs'+logids[i], {});
+					
+					msg += ' °>ID:'+logids[i]+' - Am: '+ new Date(logdata.datum)+' mit ' + logdata.uir.join(',')+'|/showlog '+logids[i]+'<° °#°'
+				}
+				
+				user.sendPrivateMessage(msg);
+				
+
+			} else {
+				user.sendPrivateMessage('Ich kenne den User nicht.');
+			}	
+		}
+	
+	}
+	
+	this.onUserLeft  = function onUserLeft(user){
+		
+		if(!logaktiv){
+			return;
+		}
+		
+		log['txt'].push('°GG°'+user.getNick().escapeKCode() + ' verlässt den Channel.°r°');
+		
+		var users			= KnuddelsServer.getChannel().getOnlineUsers(UserType.Human);
+		
+		if(users.length <= 0){
+			logaktiv = false;
+			
+			var id = KnuddelsServer.getPersistence().getNumber('LogIds', 0);
+			
+			KnuddelsServer.getPersistence().setObject('_logs'+id, log);
+			
+			var logids = KnuddelsServer.getPersistence().getObject('_logsdata', []);
+			logids.push(id);
+			KnuddelsServer.getPersistence().setObject('_logsdata', logids);
+
+		}
+		
+		
+	};
+	
+
+});
